51461cca1f9ffc5ac51fbbf3e4343f07cb2161df
jornooki <rodrigojornooki@gmail.com>
2020-10-25 20:39:50 -0200

    rodrigojornooki@gmail.com Crud Requisicao

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
-const PÒRT = process.env.PORT || 8080;
  app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
  app.get('/*', (req, res) => {
    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
  });
 
- app.listen(process.env.PORT || 80);
+ app.listen(process.env.PORT);
